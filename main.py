7659206b011a515aa423a3db99ed9b2ea0ba6cb5
Paulina Dolder <paulid4628@gmail.com>
2025-07-15 16:06:29 +0100

    Added the primary function of calculating the monthly repayments based on user input

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("Welcome to Repay-Rite!")
+from calculator import calculate_monthly_payment
+
+
+
+
+def main():
+    print("Welcome to RepayRite!")
+    
+    
+    principal = float(input("Enter the loan amount (Principal): £"))
+    rate = float(input("Enter the annual interest rate (in %): "))
+    years = int(input("Enter the loan term in years: "))
+
+    monthly_payment = calculate_monthly_payment(principal, rate, years)
+    
+    print(f"\nYour monthly payment is: £{monthly_payment:.2f}")
+
+if __name__ == "__main__":
+    main()
